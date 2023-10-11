a553087a7392895ea505f63e2bdaefaee9e25be9
Sircule14 <138093066+Sircule14@users.noreply.github.com>
2023-10-11 09:28:54 -0600

    Funcion para contar hasta un determinado número
    Función recursiva para contar hasta un determinado número.

---
+++ contar_hasta_numero.js
@@ -0,0 +1,15 @@
+/* Funcion para contar hasta un numero determinado
+con recursion */
+
+
+  function countup(n) {
+      if (n < 1) {
+        return [];
+      } else {
+        const countArray = countup(n - 1);
+        countArray.push(n);
+        return countArray;
+      }
+    }
+    console.log(countup(8));
+  
