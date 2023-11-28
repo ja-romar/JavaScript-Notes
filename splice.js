ff8eac3c081ee47640a56ecf351ba2987c6b4619
Sircule14 <victor.jromar@gmail.com>
2023-11-28 09:45:47 -0700

    Se agrega otro ejemplo de splice

--- splice.js
+++ splice.js
@@ -5,4 +5,21 @@ array.splice(7,2); //La sintaxis de splice es la siguiente
 //El primer parametro (7 en el ejemplo) es desde la posicion que eliminara elementos
 //El segundo es el numero de elementos que eliminara.
 
-console.log(array);
+console.log("Ejemplo basico de splice " + array);
+
+//Tambien podemos agregar elementos con splice de la siguiente forma
+
+
+
+
+
+
+
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi', 'watermelon'];
+
+function changes(arr) {
+    arr.splice(0,2,'strawberry','lima'); //splice que eliminara los elementos y agregara otros
+    return arr
+}
+
+console.log("ejemplo de funcion con splice " + changes(fruits)); //Mostrara el array fruits eliminando 'apple' 'banana' y agregando 'strawberry' y 'lima'
