554318e19682f3423480e1c050b0bd3c10380730
Sircule14 <victor.jromar@gmail.com>
2023-12-06 11:03:25 -0700

    Se agrega clave valor

---
+++ clave_valor.js
@@ -0,0 +1,12 @@
+
+
+const barcaPlayer = {
+	player: 'Frenkie de Jong',
+	position: 'MC',
+	nacionality: 'Dutch'
+};
+
+barcaPlayer.number = 21;
+barcaPlayer['Height'] = 1.81;
+
+console.log(barcaPlayer)
