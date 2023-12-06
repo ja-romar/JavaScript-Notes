f32e6431ae6ee6b7ff828319667f8a39e199da78
Sircule14 <victor.jromar@gmail.com>
2023-12-06 12:53:25 -0700

    iterar en claves de objeto

---
+++ iterar_claves_objeto.js
@@ -0,0 +1,12 @@
+
+//Esta funcion nos permite iterar sobre las claves de un objeto
+
+function countOnline(usersObj) {
+    let result = 0;
+    for (let user in usersObj) {
+      if (usersObj[user].online === true) {
+        result++;
+      }
+    }
+    return result;
+  }
